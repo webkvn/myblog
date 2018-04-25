aca34d611c7b88930c901095531ea612edaf019d
kevin <web_stark@163.com>
2018-04-25 09:32:40 +0800

    0425003

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const app = express()
+const indexRouter = require('./routes/index')
+const userRouter = require('./routes/users')
 
-app.get('/', function (req, res) {
-    res.send('hello, express')
-})
-
-app.get('/users/:name', function (req, res) {
-    res.send('hello, ' + req.params.name)
-})
+app.use('/', indexRouter)
+app.use('/users', userRouter)
 
 app.listen(3000)
